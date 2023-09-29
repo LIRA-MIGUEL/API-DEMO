34c42004691a3992580aeb810561420ab72f49a5
LIRA-MIGUEL Utec <1722110271@utectulancingo.edu.mx>
2023-09-29 01:40:31 +0000

    Created gitignored

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
+import csv
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-try:
-    with open("contactos.csv", "r") as archivo:
-        contenido = archivo.read()
-        print(contenido)
-        except FileNotFoundError: 
-            print("no funciona")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-    @app.get{"/v1/contactos"}
-    async get_contactos():
-        
-        #TODO read contactos.csv
-        #TODO JSON encode
-        #TODO save in response 
-        response = []
-        return response 
-        
+@app.get("/v1/contactos")
+def get_contactos():
+    datos = []
+    with open('contactos.csv', 'r', newline='') as file:
+        fieldnames = ('nombre', 'email')
+        reader = csv.DictReader(file, fieldnames)
+        for row in reader:
+            datos.append(row)
+    return datos
