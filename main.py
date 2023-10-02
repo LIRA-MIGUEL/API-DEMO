55aa7315107066039491f4e0b653e95e5c277fbc
LIRA-MIGUEL Utec <1722110271@utectulancingo.edu.mx>
2023-10-02 03:50:37 +0000

    nuevo git

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-import csv  # Importa el módulo csv
+import csv
 
 app = FastAPI()
 
@@ -14,28 +14,12 @@ class ContactoCreate(BaseModel):
     response_class=JSONResponse,
     description="Endpoint raíz de la API Contactos",
     summary="Endpoint Raíz"
-    )
+)
 async def root():
-    """
-    # Endpoint Raíz
-    ## 1-Status codes
-    * 201 - Código de muestra
-    * 334 - Otro estatus de prueba
-
-    Este es el endpoint raíz de la API de Contactos.
-    """
     return {"Hello": "World"}
 
 @app.get("/v1/contactos", response_class=JSONResponse)
 async def get_contactos():
-    """
-    # Obtener Contactos
-
-    Obtiene la lista de contactos desde un archivo CSV.
-
-    Returns:
-        List[dict]: Lista de contactos en formato JSON.
-    """
     datos = []
     with open('contactos.csv', 'r', newline='') as file:
         fieldnames = ('nombre', 'email')
@@ -44,12 +28,17 @@ async def get_contactos():
             datos.append(row)
     return datos
 
-# Agregar la operación POST sin modificar las rutas y funciones existentes
 @app.post("/v1/contactos", response_class=JSONResponse)
 async def crear_contacto(contacto: ContactoCreate):
     nuevo_contacto = {
         "nombre": contacto.nombre,
         "email": contacto.email
     }
-    # Aquí puedes agregar el código para guardar el nuevo contacto en tu archivo CSV o en tu base de datos.
+    
+    # Guardar el nuevo contacto en el archivo CSV
+    with open('contactos.csv', 'a', newline='') as file:
+        fieldnames = ['nombre', 'email']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writerow(nuevo_contacto)
+    
     return {"mensaje": "Contacto creado con éxito", "contacto": nuevo_contacto}
