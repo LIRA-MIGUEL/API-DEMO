7eebc5344511e73c7eddf610cf64be00115ed9a5
LIRA-MIGUEL Utec <1722110271@utectulancingo.edu.mx>
2023-10-05 16:53:08 +0000

    git

--- ui/dungeons.py
+++ ui/dungeons.py
@@ -9,8 +9,7 @@ print(f"GET: {response.text}")
 
 response_json = json.loads(response.text)
 
-for i in range[12]:
-
-data = response_json['results'][i]
-name = data['name']
-print(f"data: {i + 1}: {name}")
+for i in range(12):
+    data = response_json['results'][i]
+    name = ['name']
+    print(f"data {i + 1}: {name}")
